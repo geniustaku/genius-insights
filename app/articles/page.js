2e3e1e38d63be4933af72dda1e53eae0eb716cec
geniustaku <geniustakudzwa@gmail.com>
2025-04-24 20:55:42 +0200

    deploy

--- app/articles/page.js
+++ app/articles/page.js
@@ -142,7 +142,7 @@ export default async function ArticlesPage({ searchParams }) {
             type="submit"
             className="bg-indigo-600 text-white px-6 py-3 rounded-r-lg font-medium hover:bg-indigo-700 transition"
           >
-            Subscribe
+            Subscribee
           </button>
         </form>
       </div>
